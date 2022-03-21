3a477e7f64e6109f75de1e72aeb18781bdb75353
Carlos Abraao <carloschavesnd@hotmail.com>
2022-03-21 15:34:49 -0300

    criado metodo deletar

--- src/main/java/com/carlosdev/aplicacaoWeb/controller/UsuariosController.java
+++ src/main/java/com/carlosdev/aplicacaoWeb/controller/UsuariosController.java
@@ -5,10 +5,7 @@ import com.carlosdev.aplicacaoWeb.entities.Usuario;
 import com.carlosdev.aplicacaoWeb.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -47,5 +44,13 @@ public class UsuariosController {
                 usuarioRepository.save(usuario);
         return "redirect:/usuarios/listar";
 
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletar(@PathVariable(value = "id") Long id){
+        Usuario usuario = usuarioRepository.findUsuarioById(id);
+        usuarioRepository.delete(usuario);
+
+        return "redirect:/usuarios/listar";
     }
 }
--- src/main/java/com/carlosdev/aplicacaoWeb/repositories/UsuarioRepository.java
+++ src/main/java/com/carlosdev/aplicacaoWeb/repositories/UsuarioRepository.java
@@ -4,6 +4,8 @@ import com.carlosdev.aplicacaoWeb.entities.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findUsuarioById(Long id);
 }
